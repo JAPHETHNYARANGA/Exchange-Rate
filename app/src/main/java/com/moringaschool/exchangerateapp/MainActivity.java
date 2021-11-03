cf1fc54a6b1d700c6a3e8b929eb5a89c32fa6ebf
Esther-Moki <esther.moki@student.moringaschool.com>
2021-11-03 12:48:04 +0300

    changed the main activity layout

--- app/src/main/java/com/moringaschool/exchangerateapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/exchangerateapp/MainActivity.java
@@ -1,14 +1,56 @@
 package com.moringaschool.exchangerateapp;
 
+//import static androidx.collection.ArraySet.TAG;
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRatesButton;
+    private EditText mRateEditText;
+    private TextView mAppNameTextView;
+
+//    @BindView(R.id.findRatesButton) Button mFindRatesButton;
+//    @BindView(R.id.rateEditText) EditText mRateEditText;
+//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ButterKnife.bind(this);
+
+        mRateEditText = (EditText) findViewById(R.id.rateEditText);
+        mFindRatesButton = (Button) findViewById(R.id.findRatesButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+
+        mFindRatesButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRatesButton) {
+            int rate = parseInt(mRateEditText.getText().toString());
+            // Log.d(TAG, String.valueOf(rate));
+            Intent intent = new Intent(MainActivity.this, RatesActivity.class);
+            intent.putExtra("rate", rate);
+            startActivity(intent);
+        }
+
     }
 }
+
+//    parseInt(myEditWidget. getText(). toString());
--- app/src/main/java/com/moringaschool/exchangerateapp/RatesActivity.java
+++ app/src/main/java/com/moringaschool/exchangerateapp/RatesActivity.java
@@ -1,14 +1,63 @@
 package com.moringaschool.exchangerateapp;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
 
 public class RatesActivity extends AppCompatActivity {
+    private TextView mRateTextView;
+    private ListView mListView;
+
+//    @BindView(R.id.rateTextView) TextView mRateTextView;
+//    @BindView(R.id.listView) ListView mListView;
+
+   // private int [] rates  = new int [] {Integer.parseInt("dollars"), Integer.parseInt("euros")};
+    private String [] country = new String [] {"Kshs","dollars"};
+    private int [] rates  =new int[] {100,200};
+  // private String[] rates = new String[] {1000, 200,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rates);
+      //  ButterKnife.bind(this);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mRateTextView = (TextView) findViewById(R.id.rateTextView);
+
+      //  ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, currencyRates);
+        MyRatesArrayAdapter adapter = new MyRatesArrayAdapter(this, android.R.layout.simple_list_item_1, country,rates); // the arguments must match constructor's parameters!
+        mListView.setAdapter(adapter);
+
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String rates = ((TextView)view).getText().toString();
+                Toast.makeText(RatesActivity.this, rates, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        int rate = intent.getIntExtra("rate", 0);
+
+       // int rate = parseInt(intent.getStringExtra("rate"));
+        //int rate = parseInt(mRateEditText.getText().toString());
+        //int rate = intent.getStringExtra("rate");
+        mRateTextView.setText("The input currency is shs: " + rate);
     }
 }
