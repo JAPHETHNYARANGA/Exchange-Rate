0a17a09ac22ffce639d71d7c1ee3accc97c7ccb3
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-11-03 06:19:02 +0300

    Finished login and sign up

--- app/src/main/java/com/moringaschool/exchangerateapp/Authentication/LoginActivity.java
+++ app/src/main/java/com/moringaschool/exchangerateapp/Authentication/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.exchangerateapp.MainActivity;
@@ -20,7 +24,7 @@ import com.squareup.picasso.Picasso;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     ImageView facebook, twitter, google;
     TextView register;
@@ -49,21 +53,6 @@ public class LoginActivity extends AppCompatActivity {
 
 
         //logging in logic
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    finish();
-                }else{
-                    Toast.makeText(LoginActivity.this,"Sign up to continue",Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
 
 
     //login icons
@@ -91,14 +80,63 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //logging in logic
+
+       login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Login();
+
+            }
+        });
     }
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+
+    private void Login() {
+        String getEmail = email.getText().toString();
+        String getPassword = password.getText().toString();
+
+
+        if(TextUtils.isEmpty(getEmail)){
+            email.setError("Enter your Email");
+            return;
+        }else if(TextUtils.isEmpty(getPassword)) {
+            password.setError("Enter your Password");
+            return;
+
+        }else if(getPassword.length()<6){
+            password.setError("Length of password is too short!");
+            return;
+        }else if(!isValidEmail(getEmail)){
+            email.setError("Invalid Email");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(getEmail,getPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Login failed, please try again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
 
     }
 
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
 
 }
+
+
+
+
